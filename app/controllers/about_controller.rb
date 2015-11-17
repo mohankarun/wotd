89bbdb92420f5a117c7837981f96221a60df2dd6
Mohan Karunanidhi <mohan.karun@gmail.com>
2015-11-17 22:17:42 +1100

    Updated About and Read me Page

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,5 @@
+class AboutController < ApplicationController
+	def about
+	return 'about'	
+	end
+end
---
+++ config/initializers/app_config.rb
@@ -0,0 +1,2 @@
+ENV['feed_url'] = 'http://dictionary.reference.com/wordoftheday/wotd.rss'
+ENV['feed_type'] = 'dictionary'
